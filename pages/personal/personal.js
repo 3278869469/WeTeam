6f6a3cc02a71ff0eff716354189e3d7232bc7c44
前端 <1150283200@qq.com>
2021-08-26 00:08:10 +0800

    详细信息

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -5,14 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    edit: false,
+    nickname: '',
+    sex: '',
+    age: '',
+    adress: '',
+    phone: '',
+    shool: '',
+    mail: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let phone = wx.getStorageSync('phone')
+    this.setData({
+      edit: false,
+      phone: phone
+    })
+    // wx.cloud.database().collection('user').where({
+    //   phone = this.data.phone
+    // })
   },
 
   /**
@@ -62,5 +76,65 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  // 按钮，判断是否为可编辑状态，改变编辑状态
+  edit() {
+    // 编辑资料状态
+    if (this.data.edit) {
+      this.setData({
+        edit: false,
+      })
+    }
+    // 查看资料状态
+    else {
+      this.setData({
+        edit: true,
+      })
+    }
+  },
+
+  // 获取昵称
+  nicknameInput: function (e) {
+    this.setData({
+      nickname: e.detail.value
+    })
+  },
+
+  // 获取性别
+  sexInput: function (e) {
+    this.setData({
+      sex: e.detail.value
+    })
+  },
+
+  // 获取年龄
+  ageInput: function (e) {
+    this.setData({
+      age: e.detail.value
+    })
+  },
+
+  // 获取地址
+  adressInput: function (e) {
+    this.setData({
+      adress: e.detail.value
+    })
+  },
+
+  // 获取学校
+  shoolInput: function (e) {
+    this.setData({
+      shool: e.detail.value
+    })
+  },
+
+  // 获取邮箱
+  mailInput: function (e) {
+    this.setData({
+      mail: e.detail.value
+    })
+  },
+
+
 })
