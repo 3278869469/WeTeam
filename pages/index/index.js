25d19f299909dd2384e068b8b7e31887efe86207
HYX-dalao <65481990+HYX-dalao@users.noreply.github.com>
2021-08-25 16:06:43 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,15 @@
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
-})
+//index.js  
+//获取应用实例  
+var app = getApp()  
+Page({  
+  data: {  
+    movies:[  
+    {url:'https://i.niupic.com/images/2021/08/23/9sCg.png'} ,  
+    {url:'https://i.niupic.com/images/2021/08/23/9sCh.jpg'} ,  
+    {url:'https://i.niupic.com/images/2021/08/23/9sCi.png'} ,  
+    {url:'https://i.niupic.com/images/2021/08/23/9sCk.png'}   
+    ]  
+  },  
+  onLoad: function () {  
+  }  
+})  
